2e4bfbbb5c1b76a9731ccd9f671d4e65e86b739a
Daniel Kwon <dhkwon@alumni.cmu.edu>
2017-08-05 19:36:04 -0700

    Add presentational component to Task list

---
+++ frontend/components/task_list/task_list.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import TaskListItem from './task_list_item';
+import TaskForm from './task_form';
+
+class TaskList extends React.Component {
+  render() {
+    const { tasks, receiveTask } = this.props;
+    const taskItems = tasks.map(task => (
+      <TaskListItem
+        key={`task-list-item${task.id}`}
+        task={task}
+        receiveTask={ receiveTask } />
+      )
+    );
+
+    return(
+      <div>
+        <ul className="task-list">
+          { taskItems }
+        </ul>
+        <TaskForm receiveTask={ receiveTask } />
+      </div>
+    );
+  }
+}
+
+export default TaskList; 
---
+++ frontend/components/task_list/task_list_container.jsx
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import TaskList from './task_list';
+
+import { receiveTasks, receiveTask } from '../../actions/task_actions';
+import { allTasks } from '../../reducers/selectors';
+
+const mapStateToProps = state => ({
+  tasks: allTasks(state),
+  state
+});
+
+const mapDispatchToProps = dispatch => ({
+  receiveTasks: tasks => dispatch(receiveTasks(tasks)),
+  receiveTask: task => dispatch(receiveTask(task))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskList);
